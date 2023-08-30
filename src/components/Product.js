dac2cc9c88e0a03d44be9a61938bdc3d2c6abb87
webapp-master <ogundirantoluwani@gmail.com>
2023-08-30 01:06:01 +0100

    Product.js AND Ratings.js are OK and COMPLETE

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,30 +1,38 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import Rating from "../components/Rating";
+import {Link} from "react-router-dom"
 
 function Product({ product }) {
   return (
     <Card classname="my-3 p-3 rounded">
-      <a href={`/product/${product._id}`}>
-        <Card.Img src={product.image} />{" "}
-      </a>
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} />
+      </Link>
 
       <Card.Body>
-
-      <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-          </a>
+        </Link>
 
         <Card.Text as="div">
-          <div className="my-3">
-            {product.rating} from {product.numReviews} reviews
-          </div>
+            <div className="my-3">
+                {product.rating} from {product.numReviews} reviews
+            </div>
+        </Card.Text>
+        
+        <Card.Text as="h3">
+            ${product.price}
         </Card.Text>
 
+        <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+                color={"#f8e825"}
+              />
       </Card.Body>
-
-
     </Card>
   );
 }
